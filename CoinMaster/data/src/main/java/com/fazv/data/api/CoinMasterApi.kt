30fbbd6ba6891773745af81895a41f800a80e865
Fernando Vieira <f02_@hotmail.com.br>
2020-09-06 15:28:59 -0300

    Added query parameter on network interceptor

--- CoinMaster/data/src/main/java/com/fazv/data/api/CoinMasterApi.kt
+++ CoinMaster/data/src/main/java/com/fazv/data/api/CoinMasterApi.kt
@@ -1,6 +1,5 @@
 package com.fazv.data.api
 
-import com.fazv.data.BuildConfig
 import com.fazv.data.model.response.CurrenciesListResponse
 import com.fazv.data.model.response.CurrencyResponse
 import io.reactivex.Single
@@ -15,9 +14,8 @@ interface CoinMasterApi {
     fun getCurrenciesList(): Single<CurrenciesListResponse>
 
     companion object {
-        private const val QUERY_ACCESS_KEY = "?access_key=${BuildConfig.CURRENCYLAYER_ACCESS_KEY}"
         private const val QUERY_CURRENCIES = "currencies"
-        private const val CURRENCY_ENDPOINT = "live$QUERY_ACCESS_KEY"
-        private const val LIST_ENDPOINT = "list$QUERY_ACCESS_KEY"
+        private const val CURRENCY_ENDPOINT = "live"
+        private const val LIST_ENDPOINT = "list"
     }
 }
--- CoinMaster/data/src/main/java/com/fazv/data/api/retrofit/RetrofitConfig.kt
+++ CoinMaster/data/src/main/java/com/fazv/data/api/retrofit/RetrofitConfig.kt
@@ -1,6 +1,7 @@
 package com.fazv.data.api.retrofit
 
 import com.fazv.data.BuildConfig
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -25,17 +26,34 @@ class RetrofitConfig {
     private fun getHttpClient(): OkHttpClient =
         OkHttpClient
             .Builder()
-            .addInterceptor(getInterceptor())
+            .addInterceptor(getHttpLoggingInterceptor())
+            .addNetworkInterceptor(getHttpInterceptor())
             .connectTimeout(TIMEOUT, TimeUnit.SECONDS)
             .readTimeout(TIMEOUT, TimeUnit.SECONDS)
             .writeTimeout(TIMEOUT, TimeUnit.SECONDS)
             .build()
 
-    private fun getInterceptor(): HttpLoggingInterceptor =
+    private fun getHttpLoggingInterceptor(): HttpLoggingInterceptor =
         HttpLoggingInterceptor()
             .apply {
                 level = getInterceptorLevel()
             }
+
+    private fun getHttpInterceptor() = Interceptor { chain ->
+        val originalRequest = chain.request()
+        val httpUrl = originalRequest.url()
+
+        val url = httpUrl
+            .newBuilder()
+            .addQueryParameter(ACCESS_KEY_QUERY, BuildConfig.CURRENCYLAYER_ACCESS_KEY)
+            .build()
+
+        val requestBuilder = originalRequest
+            .newBuilder()
+            .url(url)
+
+        chain.proceed(requestBuilder.build())
+    }
 
     private fun getInterceptorLevel() =
         if (BuildConfig.DEBUG) {
@@ -46,5 +64,6 @@ class RetrofitConfig {
 
     companion object {
         const val TIMEOUT = 60L
+        const val ACCESS_KEY_QUERY = "access_key"
     }
 }
