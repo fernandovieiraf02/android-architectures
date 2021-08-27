9cc07fe503f8b329aad0b0141054434ac3ea63c8
Fernando Zarate <ext.fernando.vieira@dafiti.com.br>
2021-08-27 14:53:34 -0300

    first commit

--- CoinMaster/app/src/main/java/com/fazv/coinmaster/MainApplication.kt
+++ CoinMaster/app/src/main/java/com/fazv/coinmaster/MainApplication.kt
@@ -14,6 +14,7 @@ class MainApplication : Application() {
     }
 
     private fun initDagger() {
+
         component = DaggerCoinMasterComponent
             .builder()
             .application(this)
